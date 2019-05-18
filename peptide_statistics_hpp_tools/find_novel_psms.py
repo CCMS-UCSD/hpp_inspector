eaabe3a2b9f784dacd320e96427e3a30a0c4502c
Ben Pullman <bpullman@ucsd.edu>
2019-05-18 13:47:31 -0700

    update python script to run correctly

--- peptide_statistics_hpp_tools/find_novel_psms.py
+++ peptide_statistics_hpp_tools/find_novel_psms.py
@@ -17,6 +17,33 @@ def arguments():
 
 def main():
     args = arguments()
+    ids = {}
+    if args.mztab != "" and args.mztab != None:
+        for mztab_file in glob.glob(args.mztab + '/*'):
+            ids = mztab.read(mztab_file, ids)
+    peptide_to_psm = defaultdict(list)
+    for filescan,psm in ids.items():
+        peptide_to_psm[''.join([p for p in psm.sequence if p.isalpha()])].append((filescan,psm))
+        
+    with open(args.novel_coverage) as f, open(args.novel_psms) as fw:
+        header = ['protein','filename','scan','sequence','charge','type']
+        r = csv.DictReader(f, delimiter = '\t')
+        w = csv.DictWriter(fw, delimiter = '\t', fieldnames = [''])
+        for l in r:
+            supporting_peptides = l['supporting_peptides'].split(',')
+            novel_peptides = l['novel_peptides'].split(',')
+            protein = l['protein']
+            for peptide in supporting_peptides + novel_peptides:
+                for (filescan,psm) in peptide_to_psm[peptide]:
+                    w.write({
+                        'protein':protein,
+                        'filename':filescan[0],
+                        'scan':filescan[1],
+                        'sequence':psm.sequence,
+                        'charge':psm.charge,
+                        'type':'Supporting' if peptide in supporting_peptides else 'Novel'
+                    })
+
 
 
 if __name__ == '__main__':
